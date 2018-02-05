3867ac77d53ed09229a6acac98d777a84643206b
Noel Vock <noel1uk@gmail.com>
2018-02-05 16:31:28 +0000

    played with params

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require "sinatra"
 
-get '/cat' do
+get '/random-cat' do
   @name_generator = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name_generator = params[:element]
   erb(:index)
 end
