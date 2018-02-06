f2df3a729304efc4f41533c961934956e81bb5ca
Laura Chan <laurachan205@gmail.com>
2018-02-06 11:43:50 +0000

    First commit

--- app.rb
+++ app.rb
@@ -5,14 +5,23 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  p params
+get '/named-cat-with-get' do
   @name = params[:name]
   erb(:index)
+end
+# On the console: 
+# local host IP address                                           method        path              protocol
+# 127.0.0.1 - - [05/Feb/2018:20:43:02 +0000] "GET /named-cat-with-get HTTP/1.1" 200 244 0.0252
+# 127.0.0.1 - - [05/Feb/2018:20:43:06 +0000] "GET /named-cat-with-get?name=mikey HTTP/1.1" 200 88 0.0104
+
+get '/cat-form' do
+  erb(:form)
 end
 
-post '/named-cat2' do
-  p params
+post '/named-cat-with-post' do
   @name = params[:name]
   erb(:index)
 end
+# On the console:
+# 127.0.0.1 - - [05/Feb/2018:20:48:53 +0000] "GET /cat-form HTTP/1.1" 200 176 0.0239
+# 127.0.0.1 - - [05/Feb/2018:20:49:09 +0000] "POST /named-cat-with-post HTTP/1.1" 200 88 0.0298
